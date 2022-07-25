ea1b130ee3f5234eeae6632338e47527387c6a6e
umenokoudai <umenokoudai0805@gmail.com>
2022-07-25 22:53:19 +0900

    トゥルーエンドルート作成中

---
+++ Assets/EndSceneController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class EndSceneController : MonoBehaviour
+{
+    [SerializeField] Animator _anim;
+    [SerializeField] GameObject _nowScene;
+    [SerializeField] GameObject _lastScene;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "GOAL")
+        {
+            _anim.Play("SceneOut");
+        }
+    }
+    public void AnimationPlay(string animname)
+    {
+        if (_anim)
+        {
+            _anim.Play(animname);
+        }
+    }
+    public void LoadScene()
+    {
+        //SceneManager.LoadScene(scnename);
+        _nowScene.gameObject.SetActive(false);
+        _lastScene.gameObject.SetActive(true);
+    }
+}
