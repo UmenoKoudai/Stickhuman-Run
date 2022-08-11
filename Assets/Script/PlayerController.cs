8703f829c9e293c9cb518f7a6e08a95ffd13e46a
umenokoudai <umenokoudai0805@gmail.com>
2022-08-11 22:41:23 +0900

    Playerの効果音追加(スライディング、ジャンプ)

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -8,8 +8,11 @@ using System.IO;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] BoxCollider2D _up;
+    [SerializeField] AudioSource _jumpAudio;
     [SerializeField] BoxCollider2D _down;
+    [SerializeField] AudioSource _slidingAudio;
     [SerializeField] BoxCollider2D _default;
+    [SerializeField] AudioSource _defaultAudio;
     Animator _anim;
     void Start()
     {
@@ -21,6 +24,7 @@ public class PlayerController : MonoBehaviour
         _up.enabled = false;
         _down.enabled = false;
         _default.enabled = true;
+        _defaultAudio.Play();
         if (Input.GetKey(KeyCode.W))
         {
             UpMove();
@@ -37,12 +41,16 @@ public class PlayerController : MonoBehaviour
         _up.enabled = true;
         _default.enabled = false;
         _anim.Play("Jump");
+        _jumpAudio.Play();
+        _defaultAudio.Stop();
     }
     void DownMove()
     {
         _down.enabled = true;
         _default.enabled = false;
         _anim.Play("Sliding");
+        _slidingAudio.Play();
+        _defaultAudio.Stop();
     }
 }
 
